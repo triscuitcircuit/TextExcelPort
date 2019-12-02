71009c361154133e74cfdbf7cb587ca0f39f2cdf
Triscuit <54428989+Triscuit-circuit@users.noreply.github.com>
2019-12-02 10:55:35 -0500

    Add files via upload
    -error still occurs when input is overflowed
    -grid is printed and returned but no print when matched

---
+++ src/main.rs
@@ -0,0 +1,215 @@
+/*
+Copyright: Tristan Zippert
+University of Maine Computer Science
+Friday, Nov 29 2019
+*/
+#[macro_use]
+extern crate lazy_static;
+
+use std::{io, num};
+use std::borrow::Borrow;
+use std::fs;
+use std::io::{BufReader, Empty, Read};
+use std::path::Path;
+use serde::{Serialize, Deserialize};
+use serde_json;
+use std::error::Error;
+use std::fs::File;
+use core::borrow::BorrowMut;
+use std::any::Any;
+use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::thread;
+use std::rc::Rc;
+use serde_json::value::Value::Array;
+use std::sync::mpsc::{channel, Sender};
+use std::num::ParseIntError;
+
+#[derive(Debug)]
+enum SpreadsheetError {
+    ParseIntError(std::num::ParseIntError),
+    ParseFloatError(std::num::ParseFloatError),
+    MutexError,
+    IndexError,
+    NotNumberError,
+    ExitRequested,
+}
+
+lazy_static!{
+    static ref GRID: Mutex<Vec<Vec<Cell>>> = Mutex::new(vec![vec![Cell::Empty; 10]; 10]);
+}
+
+
+fn main(){
+    let game: bool = true;
+    while game{
+        let mut readin = String::new();
+        io::stdin().read_line(&mut readin).unwrap();
+        let mut iterate = readin.lines();
+        let input = iterate.next().unwrap();
+        if 2 > input.len(){
+            println!("Please enter a proper command");
+        }else{
+            match input.to_uppercase().as_ref(){
+                "LOAD"=>{
+                    println!("{}\n{:#?}","Loading...",get_grid_text())
+                },
+                _=>{
+                    match process_command(String::from(input)){
+                        Ok(output) => println!("{}",output),
+                        Err(SpreadsheetError::ExitRequested)=> std::process::exit(1),
+                        Err(SpreadsheetError::IndexError)=> println!("Index error, please try again"),
+                        Err(SpreadsheetError::MutexError)=>println!("Try again"),
+                        Err(SpreadsheetError::ParseIntError(e))=>println!("Try again"),
+                        Err(e) => eprintln!("{:#?}",e),
+                        _=> println!("{}","please enter another command again"),
+                    }
+                }
+            }
+        }
+
+    }
+
+}
+#[derive(Debug, Clone)]
+enum Cell {
+    Text(String),
+    Number(f64),
+    //Formula,
+    Empty,
+}
+impl Cell{
+    pub fn cell_text(&self) -> String {
+        match self {
+            Cell::Text(string) =>{cell_text_spaces(string.to_string().borrow())},
+            Cell::Number(number) =>{cell_text_spaces(number.to_string().borrow())},
+            //Cell::Formula(_formula_cell) => {cell_text_spaces(&self.get_value().unwrap_or(0.0).to_string())},
+            Cell::Empty => " ".repeat(10).to_string(),
+        }
+    }
+
+    pub fn full_text(&self) -> String {
+        match self {
+            Cell::Text(string) => format!("\"{}\"", string.clone()),
+            Cell::Number(number) => number.to_string(),
+            //Cell::Formula(string)=> string.get_text(),
+            Cell::Empty => String::from(" "),
+        }
+    }
+    pub fn base_text(&self)-> String{
+        match self {
+            Cell::Text(string) =>format!("\"{}\"", string.clone()),
+            Cell::Number(number) =>number.to_string(),
+            //Cell::Formula(_formula_cell) => {self.get_value().unwrap_or(0.0).to_string().to_string()},
+            Cell::Empty => " ".to_string(),
+        }
+    }
+}
+fn get_grid_text() -> Result<String,SpreadsheetError> {
+    let db = GRID.lock().map_err(|_| SpreadsheetError::MutexError)?;
+    let row: u8 = db.len() as u8;
+    let col: u8 = db[0].len() as u8;
+    let start :u8 = 65;
+    let mut top = String::from("   |");
+    let mut bottom = String::new();
+
+    for i in 0..col{
+        top.push_str(&format!("{}{}|", (start + i) as char, " ".repeat(9)));
+    }
+    for i in 0..row{
+        if i >= 9{
+            bottom.push_str(&format!("{} |", i + 1));
+        }else{
+            bottom.push_str(&format!("{}  |", i + 1 ));
+        }
+        for a in 0..col{
+            bottom.push_str(&format!("{}|",db[a as usize][i as usize].cell_text()));
+        }
+        bottom.push('\n');
+    }
+    Ok(format!("{}\n{}", top, bottom))
+}
+fn cell_text_spaces(string: &String) -> String {
+    let mut spaces = String::new();
+    for _i in 0..10 - (string.len() as i32) {
+        spaces.push(' ');
+    }
+
+    if string.len() > 10 {
+        string[0..10].parse().unwrap()
+    } else {
+        format!("{}{}", &string, spaces)
+    }
+
+}
+fn process_command(input:String) -> Result<String,SpreadsheetError>{
+    let input: Vec<&str> = input.splitn(3,' ').collect();
+    match input[0].to_uppercase().as_ref() {
+        "SORTA"=>{
+            Ok(String::from("Command Sorta entered"))
+        }
+        "SORTD"=>{
+            Ok(String::from("command Sortd entered"))
+        }
+        "CLEAR"=>{
+            if input.len() == 2{
+                let row = (input[1].to_uppercase().as_bytes()[0]-65) as usize;
+                let col_str = input[0].trim_end_matches(|c:char| !c.is_ascii_digit());
+                let col = match col_str.parse::<usize>() {
+                    Ok(num) => num -1,
+                    Err(e)=> return Err(SpreadsheetError::ParseIntError(e)),
+                };
+            }else{
+                let mut db = GRID.lock().map_err(|_| SpreadsheetError::MutexError)?;
+                let row = db.len();
+                let col = db[0].len();
+                for r in 0..row{
+                    for c in 0..col{
+                        db[r][c] = Cell::Empty;
+                    }
+                }
+            }
+            Ok(get_grid_text().expect("An error occured"))
+        }
+        "QUIT"|"STOP"=> {
+            println!("Quitting...");
+            Err(SpreadsheetError::ExitRequested)
+        }
+        "PRINT"=>{
+            match get_grid_text(){
+              Ok(out) =>{
+                  println!("{}", get_grid_text().expect("String"));
+              }
+                _=>{
+                    return Ok("".to_string())
+                }
+            };
+            Ok("".to_string())
+        }
+        _ =>{
+            let mut db = GRID.lock().map_err(|_| SpreadsheetError::MutexError)?;
+            let col:u8 = input[0].to_uppercase().as_bytes()[0] -65;
+            let row: u8 = match input[0].trim_start_matches(|c: char| !c.is_ascii_digit()).parse::<u8>() {
+              Ok(output) => output-1,
+                Err(e) => return Err(SpreadsheetError::ParseIntError(e)),
+            };
+            if input.len() >= 3{
+                let input_two = input[2];
+                if &input_two[0..1] == "("&& &input_two[input_two.len() - 1..input_two.len()] == ")"{
+                    db[row as usize][col as usize] = Cell::Text("FormulaCell".parse().unwrap());
+                }else if &input_two[0..1] == "\"" && &input_two[input_two.len() - 1..input_two.len()] == "\"" {
+                    db[row as usize][col as usize] = Cell::Text(input_two[1..input_two.len() - 1].to_string());
+                }else{
+                    db[row as usize][col as usize] = Cell::Number(match input_two.parse::<f64>(){
+                       Ok(num) => num,
+                        Err(e) => return Err(SpreadsheetError::ParseFloatError(e))
+                    });
+                }
+            }else{
+                return Ok(db[row as usize][col as usize].full_text());
+            }
+
+            return Ok(get_grid_text().expect("Spreadsheet error"))
+        }
+    }
+}

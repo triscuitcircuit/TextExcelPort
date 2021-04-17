cc8775d3701d594842a38fd0257b5388eb2021a1
Triscuit <54428989+triscuitcircuit@users.noreply.github.com>
2021-04-16 22:54:06 -0700

    Update lib.rs

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,9 +1,9 @@
 #![feature(associated_type_bounds)]
 #![feature(default_free_fn)]
-extern crate quickersort;
+//extern crate quickersort;
 extern crate lazy_static;
 extern crate yard;
-pub mod console;
+//pub mod console;
 pub mod grid;
 pub mod exec;
 pub mod structures;
